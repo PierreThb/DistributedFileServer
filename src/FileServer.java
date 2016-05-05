51cf70ff786e8fb91c785a779db7b93c2db68998
PierreThb <thube.pierre@gmail.com>
2016-05-05 18:07:19 +0100

    last commit

--- src/FileServer.java
+++ src/FileServer.java
@@ -4,25 +4,44 @@
  * and open the template in the editor.
  */
 
+<<<<<<< HEAD
 import java.io.File;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+=======
+>>>>>>> 5b58ba539e6d12417fe7e43b9db94df9053353cd
 import java.util.Map;
 
 /**
  *
  * @author Pierre
  */
-public interface FileServer extends Remote{
+public interface FileServer extends java.rmi.Remote {
 
+<<<<<<< Updated upstream:src/FileServer.java
     public String helloClient(String name) throws RemoteException;
     
+<<<<<<< HEAD
     public ArrayList<String> getFiles(String baseDir) throws RemoteException;
+=======
+    public Map getFiles(String basDir) throws RemoteException;
+=======
+    public Map getFiles(String baseDir) throws java.rmi.RemoteException;
+>>>>>>> Stashed changes:src/distributedfileserver/FileServer.java
 
-    public void createFile(String filename, boolean isDir) throws RemoteException;
+    public Map getFiles(String baseDir, boolean upper) throws java.rmi.RemoteException;
+>>>>>>> 5b58ba539e6d12417fe7e43b9db94df9053353cd
 
+    public void createFile(String filename, boolean isDir) throws java.rmi.RemoteException;
+
+<<<<<<< HEAD
     public void deleteFile(String filename) throws RemoteException;
     
     public ArrayList<String> openReadFile(String filename) throws RemoteException;
+=======
+    public void deleteFile(String filename) throws java.rmi.RemoteException;
+
+    public void openFile(String filename) throws java.rmi.RemoteException;
+>>>>>>> 5b58ba539e6d12417fe7e43b9db94df9053353cd
 }
--- src/RMIFileServer.java
+++ src/RMIFileServer.java
@@ -7,6 +7,8 @@
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 /**
  *
